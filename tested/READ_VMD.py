38eb089f7066f54ba1f66946397f526ad153e2c7
Alejandro Serda <54458284+bashlejandro@users.noreply.github.com>
2023-10-01 00:44:33 -0600

    Update READ_VMD.py

--- tested/READ_VMD.py
+++ tested/READ_VMD.py
@@ -1,28 +1,27 @@
-import os
-
 #print("Archivo para cargar, en VMD, trayectorias de dinamica formato NAMD *.coor.dcd.")
 #print("Proporcioname los siguientes datos.")
 #basedyn=input("Carpeta de trayectoria/nombrebase: ")
-print('Indica la seleccion de tu sistema segun corresponda:')
-sel_rec=input("Receptor con estructura secundaria. (Ej. chain A, protein, resid 1): ")
-sel_lig=input("Ligando. (Ej. chain B, resname UNK,...): ")
-steps=input("Steps para leer: ")
+#mol modstyle 1 0 CPK 1.000000 0.300000 12.000000 12.000000
 
+import os
+neq=input("Indicate the number of *.coor.dcd files that do not correspond to the production phase (minimization, heating, equilibrium, etc.): ")
+sel_lig=input("Indicate the atom selection of your ligand (e.g., chain B, resname UNK,...): ")
+steps=input("Steps to load your trajectory: ")
 
 f = open("READ.tcl", "w")
 f.write("""
-set files [glob *.coor.dcd]
-set list [lsort $files]
+set dcdfiles [glob *.coor.dcd]
+set dcdfiles [lsort $dcdfiles]
+set dcdfiles [lreplace $dcdfiles 0 """+str(int(neq)-1)+"""]
 foreach i $list {
 	mol addfile "$i" first 0 step """+steps+""" waitfor all
 	}
-mol selection """+sel_rec+"""
 mol modcolor 0 0 Structure
 mol modstyle 0 0 NewCartoon 0.300000 10.000000 4.100000 0
 mol selection """+sel_lig+"""
 mol addrep 0
 mol modcolor 1 0 Type
-mol modstyle 1 0 CPK 1.000000 0.300000 12.000000 12.000000
+mol modstyle 1 0 VDW 1.000000 12.0000000
 """)
 f.close()
 
